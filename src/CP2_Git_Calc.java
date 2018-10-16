f47decc2fe5c5598651fdaa0161ff4f6adf9dcd8
깃허브 이민호 <201802125@cs-cnu.org>
2018-10-16 13:17:44 +0900

    test3

--- src/CP2_Git_Calc.java
+++ src/CP2_Git_Calc.java
@@ -19,11 +19,11 @@ class Calc {
 		return a-b;
 	}
 	
-	public int mul(double a, double b) {
-		return -1;
+	public double mul(double a, double b) {
+		return a*b;
 	}
 	
-	public int div(double a, double b) {
-		return -1;
+	public double div(double a, double b) {
+		return a/b;
 	}
 }
